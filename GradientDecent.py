6deb10c19c235d2e1d0a7364a8881e5b7fba49f2
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2024-01-14 20:05:09 +0500

    added optimal code!

--- GradientDecent.py
+++ GradientDecent.py
@@ -6,26 +6,28 @@ y = torch.tensor(10.0)
 
 
 def loss(y , y_pred):
-    loss = (y - y_pred)**2
+    loss = ((y - y_pred)**2).mean()
     return loss
 
 def forward(x , w):
     return x * w
 
-def backward(w , loss):
-    loss.backward()
+def backward(w , y , y_pred):
+    calc_loss = loss(y , y_pred)
+    calc_loss.backward()
     gradient = w.grad
     update(w , gradient , 0.43)
     w.grad.zero_()
 
 def update(w , gradient , learning_rate):
     with torch.no_grad():
-        w.data = w - (learning_rate * gradient)
+        w -= (learning_rate * gradient)
 
 def train(epochs):
     for i in range(epochs):
-        print(f"epoch: {i + 1} predicted: {forward(x , w)} loss: {loss( y , forward(x , w) )} w: {w}")
-        backward(w , loss( y , forward(x , w) ))
+        y_pred = forward(x , w)
+        print(f"epoch: {i + 1} predicted: {y_pred} loss: {loss( y , y_pred )} w: {w}")
+        backward(w , y , y_pred)
 
 
 train(10)
